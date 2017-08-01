399c11daf1bca6dcecb0b17d532b1e3715c1ad77
Julian Kern <mail@juliankern.com>
2017-08-01 16:34:16 +0200

    first commit \o/

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # typeahead-tagbox
+Converts an `<input>` to a typeahead tagbox.
+
+## Basic Usage
+Javascript:
+```javascript
+typeaheadTagbox({
+    data: [
+        {"id":1,"text":"Duisburg"},
+        {"id":2,"text":"Lagonegro"},
+        {"id":3,"text":"Coquimbo"},
+        {"id":4,"text":"Kungälv"},
+        {"id":5,"text":"Brandenburg"},
+        {"id":6,"text":"Salt Lake City"},
+        {"id":7,"text":"Pomarolo"},
+        {"id":8,"text":"Hanau"},
+        {"id":9,"text":"Kampenhout"},
+        {"id":10,"text":"Matamata"}
+    ]
+});
+```
+HTML:
+```HTML
+<input type="text" name="tags" class="typeahead-tagbox">
+```
+
+## Options
+### Basic
+Name | default | type | Comment
+--- | --- | --- | ---
+caseSensitive | `false` | Boolean | If the typeahead should check the text case sensitive or not
+data | `[]` | Array | _required_ - The data which should be searched
+dropdownItemTemplate | `"{{text}}"` | String | The template for the dropdown item. Possible variables: `{{text}}`, `{{id}}`, and any additional key you add to the data objects
+idKey | `"id"` | String | Key of the data objects, by which the data should be sorted. Can be the same es `textKey`, but make sure you have no duplicates then
+inputPlaceholder | `"add..."` | String | String for the input placeholder
+minLength | `2` | Integer | Number of chars to be typed befor the search starts
+selector | `".typeahead-tagbox"` | String | Selector for the to be transformed input element
+tagDeleteButtonContent | `"x"` | String | Content of the delete button. May be an image tag or svg icon for example
+textKey | `"text"` | String | Key of the data objects, which should be displayed in the dropdown and tag
+### Classes
+Name | default | type | Comment
+--- | --- | --- | ---
+dropdownClass | `"typeahead-dropdown"` | String | The class for the dropdown element
+dropdownItemClass | `"typeahead-dropdown-item"` | String | The class for the dropdown item element
+dropdownSelectedClass | `"selected"` | String | Class to be added when a dropdown is selected by keyboard arows
+highlightClass | `"typeahead-highlight"` | String | Class to be wrapped around the matching substring
+inputClass | `"typeahead-input"` | String | Class for the actual input field
+tagClass | `"typeahead-tag"` | String | Class for the tags
+tagRemovingClass | `"removing"` | String | Class added to the tag befor removing it via keyboard backspace
+typeaheadClass | `"typeahead"` | String | Class added to the wrapper of the typeahead
+
+### Callbacks
+Name | default | type | Comment
+--- | --- | --- | ---
+onAddTag | `undefined` | undefined / null / Function | Callback to be executed when a tag is added. Parameters: `tag` which was just added, `taglist` current list of tags, including the new one
+onClickDropdownOption | `undefined` | undefined / null / Function | Callback to be extecuted when an dropdown option is clicked. Parameters: `option` which was just clicked. Executed before `onAddTag`.
+onOpenDropdown | `undefined` | undefined / null / Function | Callback to be executed when the dropdown is opened to display search results. Parameters: `value` of the input, `optionlist` which is displayed in the dropdown
+onRemoveTag | `undefined` | undefined / null / Function | Callback to be executed when a tag is removed. Parameters: `tag` which was removed, `taglist` without the just removed tag
+
+## Examples
+### Custom data & dropdown item
+```javascript
+typeaheadTagbox({
+    data: [
+        { 
+            username: 'jackson', 
+            age: 43, 
+            id: '5c678gh32', 
+            image: 'jackson-avatar.jpg' 
+        },
+        { 
+            username: 'ferdinand', 
+            age: 23, 
+            id: '5c6jh76a2', 
+            image: 'ferdinand-avatar.jpg' 
+        },
+        /* more data */
+    ],
+    textKey: 'username'
+    dropdownItemTemplate: '<img src="uploads/{{image}}"><strong>{{text}}</strong>({{age}})'
+});
+```
---
+++ src/typeahead-tagbox.js
@@ -0,0 +1,245 @@
+'use strict';
+
+export default function typeaheadTagbox(options) {
+    options = Object.assign({
+        caseSensitive: false,
+        data: [],
+        dropdownClass: 'typeahead-dropdown',
+        dropdownItemClass: 'typeahead-dropdown-item',
+        dropdownItemTemplate: '{{text}}',
+        dropdownSelectedClass: 'selected',
+        highlightClass: 'typeahead-highlight',
+        idKey: 'id',
+        inputClass: 'typeahead-input',
+        inputPlaceholder: 'add...',
+        minLength: 2,
+        onAddTag: undefined, // (tag, taglist) => {},
+        onClickDropdownOption: undefined, // (option) => {},
+        onOpenDropdown: undefined, // (value, optionlist) => {},
+        onRemoveTag: undefined, // (tag, taglist) => {},
+        selector: '.typeahead-tagbox',
+        tagClass: 'typeahead-tag',
+        tagDeleteButtonContent: 'x',
+        tagRemovingClass: 'removing',
+        textKey: 'text',
+        typeaheadClass: 'typeahead'
+    }, options);
+
+    const tagbox = $(options.selector);
+    const typeahead = document.createElement('div');
+    const dropdown = document.createElement('ul');
+    const input = document.createElement('input');
+
+    let selected = [];
+
+    let _dropdownOpen = false;
+    let _dropdownSelectedIndex = 0;
+    let _dropdownSelected;
+    let _dropdownOptions = 0;
+    let _markedRemoval;
+
+    // check if the target element is found
+    if (!tagbox) {
+        throw 'Tagbox element not found';
+    } else {
+        init();
+    }
+
+    function init() {
+        // create our required DOM structure here
+        typeahead.classList.add(options.typeaheadClass);
+        dropdown.classList.add(options.dropdownClass);
+        input.classList.add(options.inputClass);
+        input.type = 'text';
+        input.placeholder = options.inputPlaceholder;
+        tagbox.after(typeahead);
+        tagbox.type = 'hidden';
+        tagbox.remove();
+        typeahead.append(tagbox);
+        typeahead.append(input);
+        typeahead.append(dropdown);
+
+        // add some events
+        typeahead.addEventListener('click', () => {
+            input.focus();
+        });
+
+        typeahead.addEventListener('keydown', (e) => {
+            let value = e.target.value;
+
+            if (_dropdownOpen) {
+                if (e.key === 'ArrowDown' && _dropdownSelectedIndex < _dropdownOptions) {
+                    e.preventDefault();
+                    selectDropdown(++_dropdownSelectedIndex);
+                }
+
+                if (e.key === 'ArrowUp' && _dropdownSelectedIndex > 0) {
+                    e.preventDefault();
+                    selectDropdown(--_dropdownSelectedIndex);
+                }
+
+                if (e.key === 'Enter' && _dropdownSelected) {
+                    e.preventDefault();
+                    clickTypeahead(_dropdownSelected);
+                }
+            }
+
+            if (!value.length && e.key === 'Backspace') {
+                let tags = $$('.' + options.tagClass);
+                e.preventDefault();
+
+                if (!_markedRemoval) {
+                    _markedRemoval = selected[selected.length-1];
+                    tags[tags.length-1].classList.add(options.tagRemovingClass);
+                } else {
+                    removeTag(_markedRemoval);
+                    _markedRemoval = undefined;
+                }
+            } else if ($(`.${options.tagClass}.${options.tagRemovingClass}`)) {
+                _markedRemoval = undefined;
+                $(`.${options.tagClass}.${options.tagRemovingClass}`)
+                    .classList.remove(options.tagRemovingClass);
+            }
+        });
+
+        typeahead.addEventListener('input', (e) => {
+            let value = e.target.value;
+
+            if (value.length >= options.minLength) {
+                let foundData = options.data.filter((v) => {
+                    let find = v[options.textKey];
+
+                    if (!options.caseSensitive) {
+                        find = find.toLowerCase();
+                        value = value.toLowerCase();
+                    }
+
+                    return find.includes(value) && !selected.find((d) => {
+                        return d[options.idKey] === v[options.idKey];
+                    });
+                });
+
+                if (foundData.length > 0) {
+                    openDropdown(foundData, value);
+                }
+            } else if (value.length === 0) {
+                closeDropdown();
+            }
+        });
+    }
+
+    function selectDropdown(index) {
+        let newSelected = $(`.${options.dropdownItemClass}:nth-child(${index})`);
+
+        if ($(`.${options.dropdownItemClass}.${options.dropdownSelectedClass}`)) {
+            $(`.${options.dropdownItemClass}.${options.dropdownSelectedClass}`)
+                .classList.remove(options.dropdownSelectedClass);
+        }
+
+        _dropdownSelected = options.data.find((e) => {
+            return e.id === +newSelected.dataset.id;
+        });
+        newSelected.classList.add(options.dropdownSelectedClass);
+    }
+
+    function clickTypeahead(data) {
+        input.value = '';
+        addTag(data);
+        closeDropdown();
+        input.focus();
+
+        if (options.onClickDropdownOption && typeof options.onClickDropdownOption === 'function') {
+            options.onClickDropdownOption(data);
+        }
+    }
+
+    function removeTag(data) {
+        selected.splice(selected.findIndex((d) => {
+            return d[options.idKey] === data[options.idKey];
+        }), 1);
+        tagbox.value = JSON.stringify(selected);
+
+        if (options.onRemoveTag && typeof options.onRemoveTag === 'function') {
+            options.onRemoveTag(data, selected);
+        }
+
+        $(`.${options.tagClass}[data-id="${data[options.idKey]}"]`).remove();
+    }
+
+    function addTag(data) {
+        let tagElement = document.createElement('span');
+        let deleteButton = document.createElement('button');
+        let tags = $$(`.${options.tagClass}`);
+        tagElement.innerHTML = data[options.textKey];
+        deleteButton.dataset.id = data.id;
+        deleteButton.innerHTML = options.tagDeleteButtonContent;
+        tagElement.dataset.id = data[options.idKey];
+        tagElement.setAttribute('class', options.tagClass);
+        deleteButton.addEventListener('click', () => {
+            removeTag(data);
+        });
+
+        selected.push(data);
+
+        if (options.onAddTag && typeof options.onAddTag === 'function') {
+            options.onAddTag(data, selected);
+        }
+
+        tagbox.value = JSON.stringify(selected);
+
+        if (tags.length > 0) {
+            tags[$$(`.${options.tagClass}`).length - 1].after(tagElement);
+        } else {
+            typeahead.prepend(tagElement);
+        }
+
+        tagElement.append(deleteButton);
+    }
+
+    function openDropdown(data, value) {
+        let listElement;
+        dropdown.innerHTML = '';
+        data.forEach((d) => {
+            listElement = document.createElement('li');
+            listElement.classList.add(options.dropdownItemClass);
+            listElement.dataset.id = d.id;
+            listElement.innerHTML = options.dropdownItemTemplate
+                .replace(/{{text}}/g, d[options.textKey]
+                    .replace(new RegExp(value, options.caseSensitive ? 'g' : 'gi'),
+                        `<span class="${options.highlightClass}">$&</span>`)
+                )
+                .replace(/{{id}}/g, d[options.idKey])
+                .replace(/{{(\w+)}}/g, (m, p) => {
+                    return d[p] || '';
+                });
+            listElement.addEventListener('click', () => {
+                clickTypeahead(d);
+            });
+            _dropdownOptions++;
+            dropdown.append(listElement);
+        });
+        _dropdownOpen = true;
+        _dropdownSelectedIndex = 0;
+        dropdown.style.display = 'block';
+
+        if (options.onOpenDropdown && typeof options.onOpenDropdown === 'function') {
+            options.onOpenDropdown(value, data);
+        }
+    }
+
+    function closeDropdown() {
+        dropdown.innerHTML = '';
+        _dropdownOpen = false;
+        _dropdownOptions = 0;
+        _dropdownSelected = undefined;
+        dropdown.style.display = 'none';
+    }
+
+    function $(s) {
+        return document.querySelector(s);
+    }
+
+    function $$(s) {
+        return document.querySelectorAll(s);
+    }
+}
